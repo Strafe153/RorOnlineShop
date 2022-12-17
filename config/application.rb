8bd945c9533287f043192c96069bd1ffd7a59e4c
Vitalii Knysh <knish.vital@gmail.com>
2022-12-17 21:28:12 +0200

    Finished lab15

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module OnlineShop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -18,5 +19,6 @@ module OnlineShop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use I18n::Middleware
   end
 end
